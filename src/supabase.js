c48ab1a1b8844b19d82b7b736ec266de91f9ea7b
soilkane <aly.kane@staroilgroup.com>
2026-06-10 19:59:12 +0000

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://slcmshufvgbweywzqyqh.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsY21zaHVmdmdid2V5d3pxeXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMDQ2MDksImV4cCI6MjA5NjY4MDYwOX0._3LKCyK-E7NPobMb7umnt4h25id1cCjnGb04cii-4SU'
+const SUPABASE_URL = 'https://koajkvijrcxbusbhwuug.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvYWprdmlqcmN4YnVzYmh3dXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMDQ2MTYsImV4cCI6MjA5NjY4MDYxNn0.ZIwtU9HqaHJFPhNleigFUd0_0S9YH9-kjYk7ER4CQlI'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
